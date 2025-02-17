0e71c6def655903fd496c7196cd8bfc7876cdac7
batikanoz123 <batikanoz123@gmail.com>
2025-02-17 16:48:51 +0000

    Able to reject friend requests

--- Acebook/Controllers/FriendshipsController.cs
+++ Acebook/Controllers/FriendshipsController.cs
@@ -97,12 +97,12 @@ public class FriendshipsController : Controller
 
 
 
-    [Route("/Friendships/AcceptRequest")]
-    [HttpGet]
-    public IActionResult AcceptingRequest()
-    {
-        return RedirectToAction("Index", "Friendships");
-    }
+    // [Route("/Friendships/AcceptRequest")]
+    // [HttpGet]
+    // public IActionResult AcceptingRequest()
+    // {
+    //     return RedirectToAction("Index", "Friendships");
+    // }
 
 
 
@@ -118,15 +118,37 @@ public class FriendshipsController : Controller
             return RedirectToAction("Signin", "Sessions");
         }
 
-        // User? currentUser = dbContext.Users?.FirstOrDefault(u => u.Id == currentUserId.Value);
-        // if (currentUser != null)
-        // {
-        //     ViewBag.CurrentUser = currentUser;
-        //     //ViewBag.ProfileUserName = currentUser.Name;
-        // }
-
         var FriendshipToUpdate = await dbContext.Friendships.FirstOrDefaultAsync(u => u.User2Id == currentUserId.Value);;
         FriendshipToUpdate.FriendshipStatus = "Accepted";
+        dbContext.Friendships.Update(FriendshipToUpdate);
+        await dbContext.SaveChangesAsync();
+        return RedirectToAction("Index", "Friendships");
+    }
+
+
+
+    [Route("/Friendships/RejectRequest")]
+    [HttpGet]
+    public IActionResult RejectingRequest()
+    {
+        return RedirectToAction("Index", "Friendships");
+    }
+
+
+    [Route("/Friendships/RejectRequest")]
+    [HttpPost]
+    public async Task<IActionResult> RejectRequest()
+    {
+        AcebookDbContext dbContext = new AcebookDbContext();
+        int? currentUserId = HttpContext.Session.GetInt32("user_id");
+
+        if (currentUserId == null)
+        {
+            return RedirectToAction("Signin", "Sessions");
+        }
+
+        var FriendshipToUpdate = await dbContext.Friendships.FirstOrDefaultAsync(u => u.User2Id == currentUserId.Value);;
+        FriendshipToUpdate.FriendshipStatus = "Rejected";
         dbContext.Friendships.Update(FriendshipToUpdate);
         await dbContext.SaveChangesAsync();
         return RedirectToAction("Index", "Friendships");
