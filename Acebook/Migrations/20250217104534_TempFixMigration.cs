7f4130ce8b1227780c8c517b89158eb978dbe8f2
arthurbotto <arthurgsbotto@hotmail.com>
2025-02-17 10:52:49 +0000

    New migration, TempMigration, after StartCommentsLikes merge

---
+++ Acebook/Migrations/20250217104534_TempFixMigration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace acebook.Migrations
+{
+    /// <inheritdoc />
+    public partial class TempFixMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Likes",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Likes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
